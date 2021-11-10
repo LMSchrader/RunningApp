98fb101d0f2ec0efebe79f04f938cf9c3463e4b7
LMSchrader <limaschra@web.de>
2021-11-10 19:21:19 +0100

    added listener

--- app/src/main/java/com/example/runningapp/RunningScheduleActivity.kt
+++ app/src/main/java/com/example/runningapp/RunningScheduleActivity.kt
@@ -2,6 +2,7 @@ package com.example.runningapp
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.runningapp.databinding.ActivityRunningScheduleBinding
@@ -22,5 +23,10 @@ class RunningScheduleActivity : Activity() {
 
         adapter = RunningScheduleAdapter()
         binding.recyclerView.adapter = adapter
+
+        binding.fab.setOnClickListener {
+            // TODO: aendern
+            Toast.makeText(applicationContext, "replace with an action", Toast.LENGTH_LONG).show()
+        }
     }
 }
