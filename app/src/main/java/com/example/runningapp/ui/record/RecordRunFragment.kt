198b7afd58493ad3006904ef297c3c0d69674c49
LMSchrader <limaschra@web.de>
2021-11-23 17:52:14 +0100

    add permission

--- app/src/main/java/com/example/runningapp/ui/record/RecordRunFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/record/RecordRunFragment.kt
@@ -1,37 +1,81 @@
 package com.example.runningapp.ui.record
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import com.example.runningapp.R
 import com.example.runningapp.databinding.FragmentRecordRunBinding
+import com.google.android.material.snackbar.Snackbar
 
 class RecordRunFragment : Fragment() {
-
-    private lateinit var recordRunViewModel: RecordRunViewModel
     private var _binding: FragmentRecordRunBinding? = null
+    private lateinit var layout: View
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                //TODO
+                Log.i("Permission: ", "Granted")
+            } else {
+                //TODO
+                Log.i("Permission: ", "Denied")
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        recordRunViewModel =
-            ViewModelProvider(this)[RecordRunViewModel::class.java]
-
         _binding = FragmentRecordRunBinding.inflate(inflater, container, false)
+        layout = binding.root
 
-        return binding.root
+
+        binding.startButton.setOnClickListener { startRun() }
+
+        return layout
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun startRun() {
+        if (context?.let {
+                ContextCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }
+            == PackageManager.PERMISSION_GRANTED) {
+            //TODO("Not yet implemented")
+        } else requestPermission()
+    }
+
+    private fun requestPermission() {
+        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            val snackBar =
+                Snackbar.make(layout, R.string.location_permission_required, Snackbar.LENGTH_LONG)
+            snackBar.setAction(R.string.ok) {
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+            snackBar.show()
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 }
