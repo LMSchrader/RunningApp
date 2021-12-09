f8172db29689d4a89067dd0fab5a0f2f6182089d
LMSchrader <limaschra@web.de>
2021-12-09 12:17:46 +0100

    optische anpassungen

--- app/src/main/java/com/example/runningapp/util/DialogUtil.kt
+++ app/src/main/java/com/example/runningapp/util/DialogUtil.kt
@@ -3,7 +3,6 @@ package com.example.runningapp.util
 import android.app.Activity
 import android.app.Dialog
 import android.content.Context
-import android.widget.Button
 import android.widget.TextView
 import com.example.runningapp.R
 
@@ -16,12 +15,12 @@ class DialogUtil {
             dialog.setContentView(R.layout.alert_dialog)
             dialog.findViewById<TextView>(R.id.description)?.text = description
 
-            val btnCancel: Button? = dialog.findViewById(R.id.buttonCancel)
+            val btnCancel: TextView? = dialog.findViewById(R.id.buttonCancel)
             btnCancel?.setOnClickListener {
                 dialog.dismiss()
             }
 
-            val btnContinue: Button? = dialog.findViewById(R.id.buttonContinue)
+            val btnContinue: TextView? = dialog.findViewById(R.id.buttonContinue)
             btnContinue?.setOnClickListener {
                 dialog.dismiss()
                 activity.onBackPressed()
